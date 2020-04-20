df2bd098030874a908ec0cb4cbee64a8a685b793
jianjoy <13549336+jianjoy@users.noreply.github.com>
2020-04-20 11:25:59 +0800

    init

---
+++ src/test/java/com/jianjoy/web/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianjoy.web.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
